30a0bdd908c61b352569285e22c4a1cbbb0271a8
Rashid Aziz <aziz@sophire.com>
2020-04-14 11:30:52 -0400

    create world & player

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,19 +2,21 @@ extends Actor
 
 
 func _physics_process(delta):
+	var is_jump_interrupted:= Input.is_action_just_released("jump") and velocity.y < 0.0 
+	
 	var direction: = getDirection()
-	velocity = calculateMoveVelocity(velocity, direction, speed)
+	velocity = calculateMoveVelocity(velocity, direction, speed, is_jump_interrupted)
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func getDirection() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump")  else 1.0
+		-1.0 if Input.is_action_just_pressed("jump")   else 1.0
 	)
 
 func calculateMoveVelocity(
-		linearVelocity: Vector2, direction: Vector2, speed: Vector2
+		linearVelocity: Vector2, direction: Vector2, speed: Vector2, is_jump_interrupted: bool
 	) -> Vector2: 
 	var newVelocity = linearVelocity
 	
@@ -23,6 +25,7 @@ func calculateMoveVelocity(
 	newVelocity.y += gravity * get_physics_process_delta_time()
 	if direction.y == -1: 
 		newVelocity.y = speed.y * direction.y
-	
+	if is_jump_interrupted: 
+		newVelocity.y = 0.0
 	return newVelocity
 	
